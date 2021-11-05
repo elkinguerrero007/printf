40a498bb470975b346510bfe21bcfa911e476fba
elkinguerrero007 <3439@holbertonschool.com>
2021-11-05 06:42:09 +0000

    this is inicial file.h

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct printTypes - Basic structure of printf
+ * @specifier: id of char, char *, int, ...
+ * @f: Function that selects the data type
+ * Description: id and function of printf
+ */
+
+
+typedef struct printTypes
+{
+	char *specifier;
+	int (*f)();
+} pt;
+
+/* Principal */
+int _printf(const char *format, ...);
+/*endprincipal*/
+#endif
