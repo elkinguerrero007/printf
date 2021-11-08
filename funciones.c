f14ebd35ab2d2cd39263718b7105dbdc95a58862
quinto <85587286+elkinguerrero007@users.noreply.github.com>
2021-11-08 17:52:20 -0500

    agregando la funcion _abs

--- funciones.c
+++ funciones.c
@@ -1,25 +1,20 @@
-#include "main.h"
-/*Este es el archivo de subfunciones*/
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+/**                                                                                                                                
+ * _abs - Computes the absolute value of an integer                                                                                
+ *                                                                                                                                 
+ * @n: Integer parameter                                                                                                           
+ * Return: n (int)                                                                                                                 
  */
-int _putchar(char c)
+int _abs(int n)
 {
-        return (write(1, &c, 1));
+ 	if (n < 0)
+        {
+                return (n * -1);
+	}
+        else
+        {
+                return (n);
+        }
 }
-/**
- * _strlen - Count the number of chars in a string
- * @s: String given
- * Return: Length of string
- */
-int _strlen(const char *s)
-{
-	int i = 0;
-        while (s[i] != '\0')
-	        i++;
-        return (i);
-}
+
+
 
