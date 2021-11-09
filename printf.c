a26e4f91df37670f09fb9856d217d8a27359a633
Noah Verner <83596569+noahverner1995@users.noreply.github.com>
2021-11-08 19:59:51 -0500

    formato functions defined

--- printf.c
+++ printf.c
@@ -58,3 +58,65 @@ int notfound = 1;
 	va_end(args);
 	return (ans);
 }
+
+/**
+ * printChar - Prints a character
+ * @args: Argument passed
+ *
+ * Return: length of data
+ */
+int printChar(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (write(1, &c, 1));
+}
+/**
+ * printString - Prints a string
+ * @args: Argument passed
+ *
+ * Return: length of data
+ */
+int printString(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int size;
+
+	if (str == NULL)
+		str = "(null)";
+	size = _strlen(str);
+	return (write(1, str, size));
+}
+
+/**
+ * printPercentage - Prints a percent
+ * @args: Argument passed
+ *
+ * Return: length of data
+ */
+int printPercentage(va_list args)
+{
+	char p = '%';
+
+	(void) args;
+	return (write(1, &p, 1));
+}
+
+/**
+ * printInteger - Prints an integer
+ * @args: Argument passed
+ *
+ * Return: length of data
+ */
+int printInteger(va_list args)
+{
+	char buff[33];
+	char *str;
+	int num = va_arg(args, int);
+	int size;
+
+	str = itoa(num, buff, 10);
+	size = _strlen(str);
+
+	return (write(1, str, size));
+}
