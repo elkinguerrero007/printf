0c6597e88499106d15fe51fecf01e6d684fa6073
elkinguerrero007 <3439@holbertonschool.com>
2021-11-09 02:26:48 +0000

    betty style

--- printf.c
+++ printf.c
@@ -15,50 +15,47 @@ int printInteger(va_list args);
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i;
-	int ans = 0;
-	va_list args;
-	pt types[] = {
-		{"c", printChar},
-		{"s", printString},
-		{"%", printPercentage},
-		{"d", printInteger},
-		{"i", printInteger},
-		{NULL, NULL}
-	};
-	int notfound = 1;
-
-	if (format == NULL || (*format == '%' && *(format + 1) == '\0'))
-		return (-1);
-	va_start(args, format);
-
-	while (format != NULL && *format != '\0')
-	{
-		if (*format  == '%')
-		{
-			format++;
-			for (i = 0; types[i].formato != NULL; i++)
-			{
-				if (*format == *(types[i].formato))
-				{
-					ans += types[i].f(args);
-					format++;
-					notfound = 0;
-					break;
-				}
-			}
-			if (notfound)
-				ans += write(1, format - 1, 1);
-		}
-		else
-		{
-			ans += write(1, format++, 1);
-		}
-	}
-	va_end(args);
-	return (ans);
+unsigned int i;
+int ans = 0;
+va_list args;
+pt types[] = {
+{"c", printChar},
+{"s", printString},
+{"%", printPercentage},
+{"d", printInteger},
+{"i", printInteger},
+{NULL, NULL}
+};
+int notfound = 1;
+if (format == NULL || (*format == '%' && *(format + 1) == '\0'))
+return (-1);
+va_start(args, format);
+while (format != NULL && *format != '\0')
+{
+if (*format  == '%')
+{
+format++;
+for (i = 0; types[i].formato != NULL; i++)
+{
+if (*format == *(types[i].formato))
+{
+ans += types[i].f(args);
+format++;
+notfound = 0;
+break;
 }
-
+}
+if (notfound)
+ans += write(1, format - 1, 1);
+}
+else
+{
+ans += write(1, format++, 1);
+}
+}
+va_end(args);
+return (ans);
+}
 /**
  * printChar - Prints a character
  * @args: Argument passed
@@ -67,9 +64,8 @@ int _printf(const char *format, ...)
  */
 int printChar(va_list args)
 {
-	char c = va_arg(args, int);
-
-	return (write(1, &c, 1));
+char c = va_arg(args, int);
+return (write(1, &c, 1));
 }
 /**
  * printString - Prints a string
@@ -79,15 +75,13 @@ int printChar(va_list args)
  */
 int printString(va_list args)
 {
-	char *str = va_arg(args, char *);
-	int size;
-
-	if (str == NULL)
-		str = "(null)";
-	size = _strlen(str);
-	return (write(1, str, size));
+char *str = va_arg(args, char *);
+int size;
+if (str == NULL)
+str = "(null)";
+size = _strlen(str);
+return (write(1, str, size));
 }
-
 /**
  * printPercentage - Prints a percent
  * @args: Argument passed
@@ -96,12 +90,10 @@ int printString(va_list args)
  */
 int printPercentage(va_list args)
 {
-	char p = '%';
-
-	(void) args;
-	return (write(1, &p, 1));
+char p = '%';
+(void) args;
+return (write(1, &p, 1));
 }
-
 /**
  * printInteger - Prints an integer
  * @args: Argument passed
@@ -110,15 +102,13 @@ int printPercentage(va_list args)
  */
 int printInteger(va_list args)
 {
-	char buff[33];
-	char *str;
-	int num = va_arg(args, int);
-	int size;
-
-	str = itoa(num, buff, 10);
-	size = _strlen(str);
-
-	return (write(1, str, size));
+char buff[33];
+char *str;
+int num = va_arg(args, int);
+int size;
+str = itoa(num, buff, 10);
+size = _strlen(str);
+return (write(1, str, size));
 }
 /**
  * _strlen - Count the number of chars in a string
@@ -127,9 +117,8 @@ int printInteger(va_list args)
  */
 int _strlen(const char *s)
 {
-	int i = 0;
-        while (s[i] != '\0')
-	        i++;
-        return (i);
+int i = 0;
+while (s[i] != '\0')
+i++;
+return (i);
 }
-
